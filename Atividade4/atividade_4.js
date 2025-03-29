4041a0f8c34e9459bbc97792ac831e2024d2a987
Alexander Augusto de Figueiredo Baxendale <Alexander48@academicos.com>
2025-03-28 21:14:26 -0300

    Adicionando atividade 3 e 4

---
+++ Atividade4/atividade_4.js
@@ -0,0 +1,43 @@
+//Crie uma função chamada calcularTotal que receba:
+
+//•O preço unitário de um produto.
+//•A quantidade comprada.
+
+//Calcule o valor total da compra.
+
+//Crie outra função chamada aplicarDesconto que receba o valor total e retorne o valor com desconto, conforme a regra:
+
+//•Compras acima de R$ 100,00 → 10% de desconto
+//•Compras até R$ 100,00 → sem desconto
+
+
+//Adicione uma função exibirResumo() que formate e exiba um resumo final da compra.
+
+//•Valor total antes do desconto.
+//•Valor final com o desconto aplicado.
+
+//OBS: Use prompt() para ler os dados do usuário (preço e quantidade)
+
+let preco = parseFloat(prompt("Digite o preço do produto: "))
+let quant = parseFloat(prompt("Digite a Quantidade do produto"))
+function calcularTotal(preco,quant){
+    return preco*quant;
+
+}
+let total = calcularTotal(preco,quant);
+
+function aplicarDesconto(total){
+    if(total > 100){
+        total = total-(total*0.1)
+        return total;
+    }else{
+        return total
+    }
+}
+
+let exibir = aplicarDesconto(total)
+
+function exibirResumo(exibir){
+    console.log(exibir)
+}
+
---
+++ Atividade_3/atividade_3(28_03_25).js
@@ -0,0 +1,46 @@
+//1•Crie um array com 5 nomes e exiba o terceiro nome no console.
+//2•Adicione um nome ao final e um no início do array.
+//3•Remova o último nome e exiba o array atualizado.
+//4•Use map() para criar um novo array dobrando os valores de [2, 4, 6, 8].
+//5•Use filter() para criar um novo array apenas com números maiores que 5 em [1, 3, 5, 7, 9]
+
+
+//1•Adicione um nome ao final e um no início do array.
+console.log("Primeira Questão!\n")
+let nomes = ["Alexander","Augusto","Paulo","Sabrina","Maria"];
+
+console.log(nomes[2])
+
+//2•Adicione um nome ao final e um no início do array.
+console.log("\nSegunda Questão!\n")
+console.log(nomes)
+nomes.unshift("Aline")
+nomes.push("Roberto")
+
+console.log(nomes)
+
+//3•Remova o último nome e exiba o array atualizado.
+console.log("\nTerceira Questão!\n")
+console.log("\nLista Atual");
+console.log(nomes)
+console.log("\nLista sem o último elemento");
+nomes.pop()
+console.log(nomes);
+
+//4•Use map() para criar um novo array dobrando os valores de [2, 4, 6, 8]
+console.log("\nQuarta Questão!\n")
+let numeros = [2,4,6,8];
+console.log("\nLista Atual")
+console.log(numeros)
+let resultado = numeros
+console.log("Lista Dobrada")
+console.log(resultado.map(resultado => resultado*2));
+    
+//5•Use filter() para criar um novo array apenas com números maiores que 5 em [1, 3, 5, 7, 9]
+console.log("\nQuinta Questão!\n")
+let num = [1,3,5,7,9]
+console.log("\nLista sem o filtro");
+console.log(num)
+let filtra_maior_5 = num
+console.log("Lista com o filtro");
+console.log(filtra_maior_5.filter(filtra_maior_5 => filtra_maior_5 > 5));
